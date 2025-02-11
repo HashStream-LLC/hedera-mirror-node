a61c7e885827a387a09d8fe258b7de179deee821
Cory Huebner <chuebner86@gmail.com>
2025-02-11 17:40:12 -0600

    fix transaction id to use stringified account id

--- hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/contractcallnotifications/transactionmodel/TransactionIdHandler.java
+++ hedera-mirror-importer/src/main/java/com/hedera/mirror/importer/parser/record/contractcallnotifications/transactionmodel/TransactionIdHandler.java
@@ -1,12 +1,14 @@
 package com.hedera.mirror.importer.parser.record.contractcallnotifications.transactionmodel;
 
+import com.hedera.mirror.common.domain.entity.EntityId;
 import com.hederahashgraph.api.proto.java.TransactionID;
 
 public class TransactionIdHandler {
     public static String Stringify(TransactionID transactionID) {
+        String accountIdSegment = EntityId.of(transactionID.getAccountID()).toString();
         String validStartSegment = TimestampHandler.joinSecondsAndNanosWithHyphen(
                 transactionID.getTransactionValidStart()
         );
-        return String.format("%s-%s", transactionID.getAccountID(), validStartSegment);
+        return String.format("%s-%s", accountIdSegment, validStartSegment);
     }
 }
